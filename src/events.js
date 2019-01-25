d166045187815a6499d78e8a1111bc51d5021ba1
George Steel <george.steel@gmail.com>
2019-01-24 22:38:23 -0500

    refactor app initializer and create README.

--- src/events.js
+++ src/events.js
@@ -1,7 +1,9 @@
 // @flow
 import type {Component} from 'react';
 import type {ComponentId,ButtonId} from './keys';
+import {setDefaultOptions, type Options} from './options';
 import {Navigation} from 'react-native-navigation';
+import {Linking} from 'react-native';
 
 export interface EventObserver {
     +componentDidAppear?: () => void;
@@ -12,12 +14,14 @@ export interface EventObserver {
     +searchBarCancelPressed?: () => void;
 }
 
-export function bindLocalEvents<Props: {+componentId: ComponentId}>(self: Component<Props,any> & EventObserver): () => void{
+export type CancelBind = () => void;
+
+export function bindLocalEvents<Props: {+componentId: ComponentId}>(self: Component<Props,any> & EventObserver): CancelBind{
     const sub = Navigation.events().bindComponent(self);
     return sub.remove;
 }
 
-export function bindGlobalButtonPressed(key: ButtonId, onPress: () => void) {
+export function bindGlobalButtonPressed(key: ButtonId, onPress: () => void): CancelBind {
     function cb({buttonId}: {buttonId: ButtonId}) {
         if (buttonId === key) onPress();
     }
@@ -25,10 +29,43 @@ export function bindGlobalButtonPressed(key: ButtonId, onPress: () => void) {
     return sub.remove;
 }
 
+/*
 export function onAppLaunch(callback: bool => mixed) {
     let isFirstLaunch = true;
     Navigation.events().registerAppLaunchedListener(() => {
         callback(isFirstLaunch);
         isFirstLaunch = false;
+    });
+}
+*/
+
+var initCalled: bool = false;
+var pendingDeepLink: string | null = null;
+var deepLinkHandler: (string => mixed) | null = null;
+
+function processDeepLink({url}:{url: ?string}) {
+    if (!url) return;
+    else if (deepLinkHandler) deepLinkHandler(url);
+    else pendingDeepLink = url;
+}
+
+export function bindDeepLinks(handler: string => mixed): CancelBind {
+    deepLinkHandler = handler;
+    if (pendingDeepLink) handler(pendingDeepLink);
+    pendingDeepLink = null;
+    return () => {deepLinkHandler = null;};
+}
+
+export function runNavigationApp(defOptions: ?Options, init: () => mixed){
+    if (initCalled) throw Error("runNavigationApp may only be called once");
+    initCalled = true;
+    if (defOptions) setDefaultOptions(defOptions);
+    Linking.addEventListener('url', processDeepLink);
+    Navigation.events().registerAppLaunchedListener(() => {
+        if (defOptions) setDefaultOptions(defOptions);
+        pendingDeepLink = null;
+        deepLinkHandler = null;
+        Linking.getInitialURL().then(url => processDeepLink({url}));
+        init();
     });
 }
